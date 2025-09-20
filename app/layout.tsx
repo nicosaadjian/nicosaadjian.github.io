b87bf84fc8f84918a40f9deea27ee92ae4489620
nicosaadjian <nicosaadjian@gmail.com>
2025-09-19 23:41:04 -0300

    updated metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Nicolas Saadjian',
+  description: 'Nicolas Saadjian - Web Resumee'
 }
 
 export default function RootLayout({
